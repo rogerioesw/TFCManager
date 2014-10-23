d7d607b26be4b348c1b4729d63acccfac66fca5f
rogeriohalls <rogerioesw@gmail.com>
2014-10-23 19:30:03 -0200

    Alterações relatórios

--- grails-app/controllers/tfcmanager/RelatoriosController.groovy
+++ grails-app/controllers/tfcmanager/RelatoriosController.groovy
@@ -10,9 +10,100 @@ class RelatoriosController {
     def index() { }
 	
 	@Secured(['ROLE_USER','ROLE_ADMIN'])
-	def report() {
+	def anexo0() {
 		params.jasperForceDownload=true
-		params.jasperDownloadName='Teste'
+		params.jasperDownloadName='Anexo0'
+		render params as Jasper
+	}
+	
+	@Secured(['ROLE_USER','ROLE_ADMIN'])
+	def anexo1() {
+		params.jasperForceDownload=true
+		params.jasperDownloadName='Anexo1'
+		render params as Jasper
+	}
+	
+	@Secured(['ROLE_USER','ROLE_ADMIN'])
+	def anexo2() {
+		params.jasperForceDownload=true
+		params.jasperDownloadName='Anexo2'
+		render params as Jasper
+	}
+	
+	@Secured(['ROLE_USER','ROLE_ADMIN'])
+	def anexo3() {
+		params.jasperForceDownload=true
+		params.jasperDownloadName='Anexo3'
+		render params as Jasper
+	}
+	
+	@Secured(['ROLE_USER','ROLE_ADMIN'])
+	def anexo4() {
+		params.jasperForceDownload=true
+		params.jasperDownloadName='Anexo4'
+		render params as Jasper
+	}
+	
+	@Secured(['ROLE_USER','ROLE_ADMIN'])
+	def anexo5() {
+		params.jasperForceDownload=true
+		params.jasperDownloadName='Anexo5'
+		render params as Jasper
+	}
+	
+	@Secured(['ROLE_USER','ROLE_ADMIN'])
+	def anexo6() {
+		params.jasperForceDownload=true
+		params.jasperDownloadName='Anexo6'
+		render params as Jasper
+	}
+	
+	@Secured(['ROLE_USER','ROLE_ADMIN'])
+	def anexo7() {
+		params.jasperForceDownload=true
+		params.jasperDownloadName='Anexo7'
+		render params as Jasper
+	}
+	
+	@Secured(['ROLE_USER','ROLE_ADMIN'])
+	def anexo8() {
+		params.jasperForceDownload=true
+		params.jasperDownloadName='Anexo8'
+		render params as Jasper
+	}
+	
+	@Secured(['ROLE_USER','ROLE_ADMIN'])
+	def anexo9() {
+		params.jasperForceDownload=true
+		params.jasperDownloadName='Anexo9'
+		render params as Jasper
+	}
+	
+	@Secured(['ROLE_USER','ROLE_ADMIN'])
+	def anexo10() {
+		params.jasperForceDownload=true
+		params.jasperDownloadName='Anexo10'
+		render params as Jasper
+	}
+	
+	@Secured(['ROLE_USER','ROLE_ADMIN'])
+	def anexo11() {
+		params.jasperForceDownload=true
+		params.jasperDownloadName='Anexo11'
+		render params as Jasper
+	}
+	
+	@Secured(['ROLE_USER','ROLE_ADMIN'])
+	def anexo12() {
+		params.jasperForceDownload=true
+		params.jasperDownloadName='Anexo12'
+		render params as Jasper
+	}
+	
+	@Secured(['ROLE_USER','ROLE_ADMIN'])
+	def anexo121() {
+		params.jasperForceDownload=true
+		params.jasperDownloadName='Anexo121'
 		render params as Jasper
 	}
 }
--- grails-app/views/relatorios/index.gsp
+++ grails-app/views/relatorios/index.gsp
@@ -12,9 +12,88 @@
 				<li><g:link class="create" action="create"><g:message code="default.new.label" args="[entityName]" /></g:link></li>
 			</ul>
 		</div>
-		<h1>Gerar Relatórios</h1>
-		<g:form action="report" target="blank">
-			<input type="submit" value="Gerar Relatório"/>
-		</g:form>
+		<h1>Gerar Formulários</h1>
+		<table>
+			<tr>
+				<td>
+					<g:form action="anexo0" target="blank">
+						<input type="submit" value="Anexo 0"/>
+					</g:form>
+				</td>
+				<td>
+					<g:form action="anexo1" target="blank">
+						<input type="submit" value="Anexo 1"/>
+					</g:form>
+				</td>
+				<td>
+					<g:form action="anexo2" target="blank">
+						<input type="submit" value="Anexo 2"/>
+					</g:form>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<g:form action="anexo3" target="blank">
+						<input type="submit" value="Anexo 3"/>
+					</g:form>
+				</td>
+				<td>
+					<g:form action="anexo4" target="blank">
+						<input type="submit" value="Anexo 4"/>
+					</g:form>
+				</td>
+				<td>
+					<g:form action="anexo5" target="blank">
+						<input type="submit" value="Anexo 5"/>
+					</g:form>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<g:form action="anexo6" target="blank">
+						<input type="submit" value="Anexo 6"/>
+					</g:form>
+				</td>
+				<td>
+					<g:form action="anexo7" target="blank">
+						<input type="submit" value="Anexo 7"/>
+					</g:form>
+				</td>
+				<td>
+					<g:form action="anexo8" target="blank">
+						<input type="submit" value="Anexo 8"/>
+					</g:form>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<g:form action="anexo9" target="blank">
+						<input type="submit" value="Anexo 9"/>
+					</g:form>
+				</td>
+				<td>
+					<g:form action="anexo10" target="blank">
+						<input type="submit" value="Anexo 10"/>
+					</g:form>
+				</td>
+				<td>
+					<g:form action="anexo11" target="blank">
+						<input type="submit" value="Anexo 11"/>
+					</g:form>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<g:form action="anexo12" target="blank">
+						<input type="submit" value="Anexo 12"/>
+					</g:form>
+				</td>
+				<td>
+					<g:form action="anexo121" target="blank">
+						<input type="submit" value="Anexo 12.1"/>
+					</g:form>
+				</td>
+			</tr>
+		</table>
 	</body>
 </html>
